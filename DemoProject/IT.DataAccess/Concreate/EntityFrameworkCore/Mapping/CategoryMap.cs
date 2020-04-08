e744d6e383ee288af8863946b575cccc87d15fe5
ismailtepeli <43654629+ismailtepeli@users.noreply.github.com>
2020-04-08 23:14:10 +0300

    Mapping işlemi

---
+++ DemoProject/IT.DataAccess/Concreate/EntityFrameworkCore/Mapping/CategoryMap.cs
@@ -0,0 +1,30 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using IT.Entity.Concrete;
+using System.Linq;
+
+
+
+namespace IT.DataAccess.Concreate.EntityFrameworkCore.Mapping
+{
+    public class CategoryMap : IEntityTypeConfiguration<Category>
+    {
+       
+
+       public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToTable(@"Category", @"dbo");
+            builder.HasKey(c => c.Id);
+
+
+            builder.Property(c => c.AddedBy).HasColumnName("AddedBy       ");
+            builder.Property(c => c.AddedDate).HasColumnName("AddedDate");
+            builder.Property(c => c.isActive).HasColumnName("isActive");
+            builder.Property(c => c.Name).HasColumnName("Name");
+        }
+    }
+}
---
+++ DemoProject/IT.DataAccess/Concreate/EntityFrameworkCore/Mapping/ProductMap.cs
@@ -0,0 +1,27 @@
+﻿using IT.Entity.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IT.DataAccess.Concreate.EntityFrameworkCore.Mapping
+{
+    public class ProductMap : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.ToTable("Product", "dbo");
+
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.AddedBy).HasColumnName("AddedBy");
+            builder.Property(p => p.AddedDate).HasColumnName("AddedDate");
+            builder.Property(p => p.CategoryId).HasColumnName("CategoryId");
+            builder.Property(p => p.Explanation).HasColumnName("Explanation");
+            builder.Property(p => p.Height).HasColumnName("Height");
+            builder.Property(p => p.Weight).HasColumnName("Weight");
+            builder.Property(p => p.Width).HasColumnName("Width");
+        }
+    }
+}
