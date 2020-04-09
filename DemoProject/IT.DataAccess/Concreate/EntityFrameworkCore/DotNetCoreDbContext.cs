68773efdab93552ccaf48b02d97ea04ac8206703
ismailtepeli <43654629+ismailtepeli@users.noreply.github.com>
2020-04-10 01:07:12 +0300

    DbContext

---
+++ DemoProject/IT.DataAccess/Concreate/EntityFrameworkCore/DotNetCoreDbContext.cs
@@ -0,0 +1,18 @@
+﻿using IT.Entity.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IT.DataAccess.Concreate.EntityFrameworkCore
+{
+    public class DotNetCoreDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-Q3FAR2U\SQLEXPRESS;Database=DotNetCoreDB;integrated security=true;Connection Timeout=1800;MultipleActiveResultSet=True");
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
