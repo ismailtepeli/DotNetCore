756eac0b2ab92463f9664b7c1d9d1462c774557d
ismailtepeli <43654629+ismailtepeli@users.noreply.github.com>
2020-04-11 21:12:58 +0300

    Bussines Katmanı

---
+++ DemoProject/IT.Bussiness/Abstract/ICategoryServices.cs
@@ -0,0 +1,26 @@
+﻿using IT.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT.Bussiness.Abstract
+{
+    public interface ICategoryServices
+    {
+        Category Add(Category category);
+
+        Task<Category> AddAsync(Category category);
+
+        Category Update(Category category);
+
+        Task<Category> UpdateAsync(Category category);
+
+        void Delete(Category category);
+
+        Category GetById(int Id);
+
+        List<Category> GetList();
+
+    }
+}
---
+++ DemoProject/IT.Bussiness/Abstract/IProductServices.cs
@@ -0,0 +1,27 @@
+﻿using IT.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT.Bussiness.Abstract
+{
+    public interface IProductServices
+    {
+        Product Add(Product product);
+
+        Task<Product> AddAsync(Product product);
+
+        Product Update(Product product);
+
+        Product UpdateAync(Product product);
+
+        void Delete(Product product);
+
+        Product GetById(int Id);
+
+        List<Product> GetList();
+
+        List<Product> GetListByCategoryId(int CategoryId);
+    }
+}
